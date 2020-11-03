f94f892b152ed19048fdc372f112d33bc5e9490a
Taishi Roy <44529756+TaishiRoy@users.noreply.github.com>
2020-11-03 12:51:33 +0530

    First Commit

---
+++ Commonly Asked/Trim_BST.cpp
@@ -0,0 +1,34 @@
+
+// function to Trim a Binary Search Tree within a range
+// O(n) time complexity
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(!root)   return root;
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        if(root->val<low)   
+        {
+            return root->right;
+        }
+        if(root->val>high)  
+        {
+            return root->left;
+        }
+        return root;
+    }
+};
